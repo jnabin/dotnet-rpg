56375c4c7d2a054c91117085420a30db0e834e3d
jnabin663 <jahangir@freightoscope.com>
2024-10-30 13:45:52 +0600

    get post implementaion with DbContext

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using dotnet_rpg.Data;
 using dotnet_rpg.dtos;
 using dotnet_rpg.dtos.Character;
+using Microsoft.EntityFrameworkCore;
 
 namespace dotnet_rpg.Services.CharacterService
 {
@@ -14,32 +16,35 @@ namespace dotnet_rpg.Services.CharacterService
             new Character(),
             new Character{Id = 1, Name = "Demo"}
         };
+        public DataContext _context;
 
         private IMapper _mapper;
-        public CharacterService(IMapper mapper)
+        public CharacterService(IMapper mapper, DataContext context)
         {
+            _context = context;
             _mapper = mapper;
         }
         public async Task<ServiceRespose<List<GetCharacterDto>>> addCharacter(AddCharacterDto character)
         {
             var mainCharacter = _mapper.Map<Character>(character);
-            mainCharacter.Id = characters.Max(x => x.Id)+1;
-            characters.Add(mainCharacter);
-            var characterDto = characters.Select(x => _mapper.Map<GetCharacterDto>(x)).ToList();
+            _context.Characters.Add(mainCharacter);
+            await _context.SaveChangesAsync();
+            var characterDto = await _context.Characters.Select(x => _mapper.Map<GetCharacterDto>(x)).ToListAsync();
             ServiceRespose<List<GetCharacterDto>> serviceRespose = new ServiceRespose<List<GetCharacterDto>>{Data = characterDto};
             return serviceRespose;
         }
 
         public async Task<ServiceRespose<List<GetCharacterDto>>> getAll()
         {
-             var characterDto = characters.Select(x => _mapper.Map<GetCharacterDto>(x)).ToList();
+            var dbCharacters = await _context.Characters.ToListAsync();
+            var characterDto = dbCharacters.Select(x => _mapper.Map<GetCharacterDto>(x)).ToList();
             ServiceRespose<List<GetCharacterDto>> serviceRespose = new ServiceRespose<List<GetCharacterDto>>{Data = characterDto};
             return serviceRespose;
         }
 
         public async Task<ServiceRespose<GetCharacterDto>> getSingle(int id)
         {
-            var character = characters.FirstOrDefault(x => x.Id == id);
+            var character = await _context.Characters.FirstOrDefaultAsync(x => x.Id == id);
             ServiceRespose<GetCharacterDto> serviceRespose = new ServiceRespose<GetCharacterDto>{Data = _mapper.Map<GetCharacterDto>(character)};
             return serviceRespose;
         }
