047224b52cb80706aaa15112638fbbef91583340
jnabin663 <jahangir@freightoscope.com>
2024-10-31 22:12:14 +0600

    user registration

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_rpg.Data;
+using dotnet_rpg.dtos.AuthRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class AuthController: ControllerBase
+    {
+        private IAuthRepository _authRepository;
+        public AuthController(IAuthRepository authRepository)
+        {
+            _authRepository = authRepository;
+            
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<ServiceRespose<int>>> Register(RegisterDto request){
+            var response = await _authRepository.Register(
+                new User(){UserName = request.Username}, request.Password
+            );
+
+            if(response.Success){
+                return Ok(response);
+            } else{
+                return BadRequest(response);
+            }
+        }
+    }
+}
